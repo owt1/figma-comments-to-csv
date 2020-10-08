2a73c23db3d57061f7f2785a55aca3fdbcc59913
ellreka <ellreka5364@gmail.com>
2020-10-09 02:26:41 +0900

    feat

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import fetch from "node-fetch";
 import * as stringifySync from "csv-stringify/lib/sync";
 import * as fs from "fs";
+import * as yargs from "yargs";
 
 interface Comment {
   id: string;
@@ -10,7 +11,13 @@ interface Comment {
   message: string;
   url: string;
   created_at: string;
+  file_key: string;
 }
+
+const argv = yargs.options({
+  key: { type: "string", description: "figma file key", demandOption: true },
+  token: { type: "string", description: "figma token", demandOption: true },
+}).argv;
 
 async function getComments({ key, token }: Record<"key" | "token", string>) {
   const comments = await fetch(
@@ -29,35 +36,41 @@ function generateCsv(comments: Comment[]) {
   const csvString = stringifySync(comments, {
     header: true,
     columns: {
-      id: "ID",
-      order_id: "pin_id",
+      id: "id",
+      order_id: "order_id",
       parent_id: "parent_id",
       name: "name",
       message: "message",
       url: "url",
       created_at: "created_at",
+      file_key: "file_key",
     },
     quoted_string: true,
   });
-  fs.writeFileSync(`figma_comments.csv`, csvString);
+  fs.writeFileSync(`figma_comments_${comments[0].file_key}.csv`, csvString);
 }
 
-getComments({
-  key: "",
-  token: "",
-}).then((res) => {
-  const comments = res.comments.map((comment) => {
-    return {
-      id: comment.id,
-      order_id: comment.order_id,
-      parent_id: comment.parent_id,
-      name: comment.user.handle,
-      message: comment.message,
-      url: `https://www.figma.com/file/${comment.file_key}?node-id=${
-        comment.client_meta?.node_id
-      }#${comment.order_id === null ? comment.parent_id : comment.id}`,
-      created_at: comment.created_at,
-    };
+function main() {
+  getComments({
+    key: argv.key,
+    token: argv.token,
+  }).then((res) => {
+    const comments = res.comments.map((comment) => {
+      return {
+        id: comment.id,
+        order_id: comment.order_id,
+        parent_id: comment.parent_id,
+        name: comment.user.handle,
+        message: comment.message,
+        url: `https://www.figma.com/file/${comment.file_key}?node-id=${
+          comment.client_meta?.node_id
+        }#${comment.order_id === null ? comment.parent_id : comment.id}`,
+        created_at: comment.created_at,
+        file_key: comment.file_key,
+      };
+    });
+    generateCsv(comments);
   });
-  generateCsv(comments);
-});
+}
+
+module.exports = main;
